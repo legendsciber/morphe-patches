cbaa974053728e2e93d291db6ab7e4522f547900
legendsciber <legendsciber@users.noreply.github.com>
2026-09-02 00:28:13 +0000

    fix(shadowfight): fix DumpHelper ClassNotFound via IOUtils readInputStreamFully

--- patches/src/main/kotlin/app/shadowfight/patches/dump/DumpPatch.kt
+++ patches/src/main/kotlin/app/shadowfight/patches/dump/DumpPatch.kt
@@ -17,9 +17,6 @@ val sfDumpPatch = rawResourcePatch(
         val soFile = get("assets/libShadowDump.so", true)
         val soBytes = DumpSoBytes.part0() + DumpSoBytes.part1() + DumpSoBytes.part2() + DumpSoBytes.part3() + DumpSoBytes.part4() + DumpSoBytes.part5() + DumpSoBytes.part6() + DumpSoBytes.part7() + DumpSoBytes.part8() + DumpSoBytes.part9()
         soFile.writeBytes(soBytes)
-        val dexFile = get("classes9.dex", true)
-        val dexBytes = DumpHelperBytes.part0() + DumpHelperBytes.part1()
-        dexFile.writeBytes(dexBytes)
     }
 }
 
@@ -32,7 +29,26 @@ val sfDumpTriggerPatch = bytecodePatch(
     compatibleWith(COMPATIBILITY_SF2)
     execute {
         UnityOnCreateFingerprint.method.addInstructions(0, """
-            invoke-static {p0}, Lhelper/DumpHelper;->load(Landroid/content/Context;)V
+            const-string v0, "libShadowDump.so"
+            invoke-virtual {p0}, Landroid/content/Context;->getAssets()Landroid/content/res/AssetManager;
+            move-result-object v1
+            invoke-virtual {v1, v0}, Landroid/content/res/AssetManager;->open(Ljava/lang/String;)Ljava/io/InputStream;
+            move-result-object v1
+            invoke-static {v1}, Lcom/google/android/gms/common/util/IOUtils;->readInputStreamFully(Ljava/io/InputStream;)[B
+            move-result-object v0
+            invoke-virtual {v1}, Ljava/io/InputStream;->close()V
+            invoke-virtual {p0}, Landroid/content/Context;->getFilesDir()Ljava/io/File;
+            move-result-object v1
+            new-instance v2, Ljava/io/File;
+            const-string v3, "libShadowDump.so"
+            invoke-direct {v2, v1, v3}, Ljava/io/File;-><init>(Ljava/io/File;Ljava/lang/String;)V
+            invoke-virtual {v2}, Ljava/io/File;->getAbsolutePath()Ljava/lang/String;
+            move-result-object v3
+            new-instance v1, Ljava/io/FileOutputStream;
+            invoke-direct {v1, v2}, Ljava/io/FileOutputStream;-><init>(Ljava/io/File;)V
+            invoke-virtual {v1, v0}, Ljava/io/FileOutputStream;->write([B)V
+            invoke-virtual {v1}, Ljava/io/FileOutputStream;->close()V
+            invoke-static {v3}, Ljava/lang/System;->load(Ljava/lang/String;)V
         """.trimIndent())
     }
 }
