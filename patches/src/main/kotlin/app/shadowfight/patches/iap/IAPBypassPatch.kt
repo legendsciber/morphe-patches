bdac586af6e71891e4720306ac37749763ad7b3e
legendsciber <legendsciber@users.noreply.github.com>
2026-09-08 22:46:41 +0000

    fix(iap): remove blank lines from smali that crash InlineSmaliCompiler

--- patches/src/main/kotlin/app/shadowfight/patches/iap/IAPBypassPatch.kt
+++ patches/src/main/kotlin/app/shadowfight/patches/iap/IAPBypassPatch.kt
@@ -36,79 +36,42 @@ val sfIAPBypassSmaliPatch = bytecodePatch(
     execute {
         IAPBypassSmaliFingerprint.method.addInstructionsWithLabels(0, """
             invoke-virtual {p2}, Lcom/android/billingclient/api/BillingFlowParams;->zzh()Ljava/util/List;
-
             move-result-object v0
-
             if-eqz v0, :fallback_error
-
             invoke-interface {v0}, Ljava/util/List;->size()I
-
             move-result v1
-
             if-lez v1, :fallback_error
-
             const/4 v1, 0x0
-
             invoke-interface {v0, v1}, Ljava/util/List;->get(I)Ljava/lang/Object;
-
             move-result-object v0
-
             check-cast v0, Lcom/android/billingclient/api/BillingFlowParams${'$'}ProductDetailsParams;
-
             invoke-virtual {v0}, Lcom/android/billingclient/api/BillingFlowParams${'$'}ProductDetailsParams;->zza()Lcom/android/billingclient/api/ProductDetails;
-
             move-result-object v0
-
             invoke-virtual {v0}, Lcom/android/billingclient/api/ProductDetails;->getProductId()Ljava/lang/String;
-
             move-result-object v0
-
             new-instance v1, Ljava/lang/StringBuilder;
-
             invoke-direct {v1}, Ljava/lang/StringBuilder;-><init>()V
-
             const-string v2, "{\"orderId\":\"morphe_bypass\",\"packageName\":\"com.nekki.shadowfight\",\"productIds\":[\""
-
             invoke-virtual {v1, v2}, Ljava/lang/StringBuilder;->append(Ljava/lang/String;)Ljava/lang/StringBuilder;
-
             invoke-virtual {v1, v0}, Ljava/lang/StringBuilder;->append(Ljava/lang/String;)Ljava/lang/StringBuilder;
-
             const-string v2, "\"],\"purchaseTime\":0,\"purchaseState\":0,\"purchaseToken\":\"morphe_bypass_token\"}"
-
             invoke-virtual {v1, v2}, Ljava/lang/StringBuilder;->append(Ljava/lang/String;)Ljava/lang/StringBuilder;
-
             invoke-virtual {v1}, Ljava/lang/StringBuilder;->toString()Ljava/lang/String;
-
             move-result-object v0
-
             new-instance v1, Lcom/android/billingclient/api/Purchase;
-
             const-string v2, ""
-
             invoke-direct {v1, v0, v2}, Lcom/android/billingclient/api/Purchase;-><init>(Ljava/lang/String;Ljava/lang/String;)V
-
             new-instance v0, Ljava/util/ArrayList;
-
             invoke-direct {v0}, Ljava/util/ArrayList;-><init>()V
-
             invoke-virtual {v0, v1}, Ljava/util/ArrayList;->add(Ljava/lang/Object;)Z
-
             iget-object v1, p0, Lcom/android/billingclient/api/BillingClientImpl;->zze:Lcom/android/billingclient/api/zzn;
-
             if-eqz v1, :fallback_error
-
             invoke-virtual {v1}, Lcom/android/billingclient/api/zzn;->zzd()Lcom/android/billingclient/api/PurchasesUpdatedListener;
-
             move-result-object v1
-
             if-eqz v1, :fallback_error
-
             sget-object v2, Lcom/android/billingclient/api/zzcj;->zzl:Lcom/android/billingclient/api/BillingResult;
-
             invoke-interface {v1, v2, v0}, Lcom/android/billingclient/api/PurchasesUpdatedListener;->onPurchasesUpdated(Lcom/android/billingclient/api/BillingResult;Ljava/util/List;)V
-
             return-object v2
-
             :fallback_error
             sget-object v0, Lcom/android/billingclient/api/zzcj;->zzl:Lcom/android/billingclient/api/BillingResult;
             return-object v0
