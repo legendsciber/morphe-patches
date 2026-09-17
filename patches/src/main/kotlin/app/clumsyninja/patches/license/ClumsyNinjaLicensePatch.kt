0a51b96657d057e40e2ee767e659a8f08ccd2e2a
legendsciber <legendsciber@users.noreply.github.com>
2026-09-17 13:39:09 +0000

    fix(clumsyninja): fix smali string escaping for inner class reference

--- patches/src/main/kotlin/app/clumsyninja/patches/license/ClumsyNinjaLicensePatch.kt
+++ patches/src/main/kotlin/app/clumsyninja/patches/license/ClumsyNinjaLicensePatch.kt
@@ -4,6 +4,8 @@ import app.morphe.patcher.extensions.InstructionExtensions.addInstructions
 import app.morphe.patcher.patch.bytecodePatch
 import app.clumsyninja.patches.shared.Constants.COMPATIBILITY_CLUMSYNINJA
 
+private const val CALLBACK_CLASS = "Lorg/naturalmotion/NmgSystem/NmgMarketplaceGooglePlayApkExpansion\$1;"
+
 @Suppress("unused")
 val clumsyNinjaLicensePatch = bytecodePatch(
     name = "Clumsy Ninja License Bypass",
@@ -14,12 +16,12 @@ val clumsyNinjaLicensePatch = bytecodePatch(
 
     execute {
         DontAllowFingerprint.method.addInstructions(0, """
-            invoke-virtual {p0, p1}, Lorg/naturalmotion/NmgSystem/NmgMarketplaceGooglePlayApkExpansion\$1;->allow(I)V
+            invoke-virtual {p0, p1}, $CALLBACK_CLASS->allow(I)V
             return-void
         """.trimIndent())
 
         ApplicationErrorFingerprint.method.addInstructions(0, """
-            invoke-virtual {p0, p1}, Lorg/naturalmotion/NmgSystem/NmgMarketplaceGooglePlayApkExpansion\$1;->allow(I)V
+            invoke-virtual {p0, p1}, $CALLBACK_CLASS->allow(I)V
             return-void
         """.trimIndent())
     }
