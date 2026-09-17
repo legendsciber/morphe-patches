0239066ad2f96c468ad334fc47dba66a547d9225
legendsciber <legendsciber@users.noreply.github.com>
2026-09-17 17:46:44 +0000

    fix(clumsy-ninja): add Google Play license bypass
    - Redirect dontAllow and applicationError callbacks to allow
    - Fixes 'authentication process has failed' error on startup

--- patches/src/main/kotlin/app/clumsyninja/patches/permission/Fingerprints.kt
+++ patches/src/main/kotlin/app/clumsyninja/patches/permission/Fingerprints.kt
@@ -12,3 +12,23 @@ object QueryPermissionFingerprint : Fingerprint(
         string("QueryPermission: ")
     )
 )
+
+object DontAllowFingerprint : Fingerprint(
+    definingClass = "Lorg/naturalmotion/NmgSystem/NmgMarketplaceGooglePlayApkExpansion\$1;",
+    name = "dontAllow",
+    returnType = "V",
+    parameters = listOf("I"),
+    filters = listOf(
+        string("Authentication rejected.")
+    )
+)
+
+object ApplicationErrorFingerprint : Fingerprint(
+    definingClass = "Lorg/naturalmotion/NmgSystem/NmgMarketplaceGooglePlayApkExpansion\$1;",
+    name = "applicationError",
+    returnType = "V",
+    parameters = listOf("I"),
+    filters = listOf(
+        string("ERROR_INVALID_PACKAGE_NAME")
+    )
+)
